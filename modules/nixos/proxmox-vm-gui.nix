4b207d3d48fcde3a246452f16384b50971a5d440
Dmytro Kyrychuk <dmytro@kyrych.uk>
2023-05-20 10:30:41 +0000

    Remove qxl video driver
    It was causing cursor flickering

--- modules/nixos/proxmox-vm-gui.nix
+++ modules/nixos/proxmox-vm-gui.nix
@@ -1,11 +1,5 @@
 { config, lib, pkgs, ... }:
 {
-  environment.systemPackages = [
-    pkgs.xorg.xf86videoqxl
-  ];
-
-  services.xserver.videoDrivers = [ "qxl" ];
-
   services.qemuGuest.enable = true;
   services.spice-vdagentd.enable = true;
 }

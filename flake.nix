8dc00dea5d97da6cc7fd9f19626a18a00e4917b3
Dmytro Kyrychuk <dmytro@kyrych.uk>
2023-10-04 15:48:55 +0000

    Remove unused systems

--- flake.nix
+++ flake.nix
@@ -16,11 +16,7 @@
     let
       inherit (self) outputs;
       forAllSystems = nixpkgs.lib.genAttrs [
-        "aarch64-linux"
-        "i686-linux"
         "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
       ];
     in
     rec {
